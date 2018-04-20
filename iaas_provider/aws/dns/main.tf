cd481b6bb853e0cfa08aa3911fb184ee538b1400
davidjeddy <me@davidjeddy.com>
2018-04-20 15:59:26 -0400

    MOVED DNS to a module

---
+++ iaas_provider/aws/dns/main.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "www" {
+  zone_id = "Z1DZELSU4ROKDZ"
+  name    = "davidjeddy.me"
+  type    = "A"
+  ttl     = "60"
+  records = ["${aws_eip.wordpress_eip.public_ip}"]
+}
--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ module "compute" {
   source = "./iaas_provider/aws/compute"
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = "Z1DZELSU4ROKDZ"
-  name    = "davidjeddy.me"
-  type    = "A"
-  ttl     = "60"
-  records = ["${aws_eip.wordpress_eip.public_ip}"]
+module "dns" {
+  source = "./iaas_provider/aws/compute"
 }
