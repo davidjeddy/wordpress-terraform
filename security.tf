f86e8712147650c7463ed2f5dc18bf4ff8ebf81a
davidjeddy <me@davidjeddy.com>
2018-04-20 12:07:38 -0400

    ADDED ICMP e/ingress configuration.

--- security.tf
+++ security.tf
@@ -3,6 +3,20 @@ resource "aws_security_group" "wordpress_security_group" {
   description = "Control the access to the wordpress server."
 }
 
+# TCP
+
+# INGRESS
+
+# allow ssh access from port 22 from variables.tfvars::var.local_ip
+resource "aws_security_group_rule" "ingress_ssh" {
+  security_group_id = "${aws_security_group.wordpress_security_group.id}"
+  type = "egress"
+  cidr_blocks = ["${var.local_ip}/32"]
+  protocol = "tcp"
+  from_port = 22
+  to_port = 22
+}
+
 # allow http access on port 80 for all address/ports
 resource "aws_security_group_rule" "ingress_http" {
   security_group_id = "${aws_security_group.wordpress_security_group.id}"
@@ -14,7 +28,7 @@ resource "aws_security_group_rule" "ingress_http" {
 }
 
 # allow https access on port 443 for all address/ports
-resource "aws_security_group_rule" "ingress" {
+resource "aws_security_group_rule" "ingress_https" {
   security_group_id = "${aws_security_group.wordpress_security_group.id}"
   type = "ingress"
   cidr_blocks = ["0.0.0.0/0"]
@@ -23,7 +37,7 @@ resource "aws_security_group_rule" "ingress" {
   to_port = 443
 }
 
-# allow reply traffic from the intent ro the server on ephemeral ports
+# allow reply traffic from the internet to the server on ephemeral ports
 resource "aws_security_group_rule" "ingress_reply" {
   security_group_id = "${aws_security_group.wordpress_security_group.id}"
   type = "ingress"
@@ -33,22 +47,38 @@ resource "aws_security_group_rule" "ingress_reply" {
   to_port = 65535
 }
 
-# allow reply traffic from the intent ro the server on ephemeral ports
+# EXGRESS
+
+# allow reply traffic from the server to the internet on ephemeral ports
 resource "aws_security_group_rule" "engress_reply" {
   security_group_id = "${aws_security_group.wordpress_security_group.id}"
   type = "egress"
   cidr_blocks = ["0.0.0.0/0"]
   protocol = "all"
-  from_port = 0
+  from_port = 1024
+  to_port = 65535
+}
+
+# ICMP
+
+# INGRESS
+
+# allow ICMP access from the internet
+resource "aws_security_group_rule" "ingress_icmp" {
+  security_group_id = "${aws_security_group.wordpress_security_group.id}"
+  type = "ingress"
+  cidr_blocks = ["0.0.0.0/0"]
+  protocol = "icmp"
+  from_port = 8
   to_port = 0
 }
 
-# allow ssh access from port 22 from variables.tfvars::var.local_ip
-resource "aws_security_group_rule" "ingress_ssh" {
+# EGRESS
+resource "aws_security_group_rule" "egress_icmp" {
   security_group_id = "${aws_security_group.wordpress_security_group.id}"
   type = "egress"
-  cidr_blocks = ["${var.local_ip}/32"]
-  protocol = "tcp"
-  from_port = 22
-  to_port = 22
+  cidr_blocks = ["0.0.0.0/0"]
+  protocol = "icmp"
+  from_port = 8
+  to_port = 0
 }
