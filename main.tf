e2efd6320b1d6219466e5f1abe921bfc21f647ae
davidjeddy <me@davidjeddy.com>
2018-04-20 14:32:13 -0400

    ADDED main.tf to many sub-dirs. This makes them a module to TF and allows importing to other TF condifuration files. Moves provider to its own dir.

--- main.tf
+++ main.tf
@@ -1,2 +1,22 @@
 # Main TF
 # Import modules as needed and configure herein
+
+module "aws_ec2" {
+  source ="./iaas_service/aws/ec2"
+}
+
+module "outputs" {
+  source ="./outputs"
+}
+
+module "provider" {
+  source ="./provider"
+}
+
+module "route_53" {
+  source ="./iaas_service/aws/ec2"
+}
+
+module "variables" {
+  source ="./variables"
+}
