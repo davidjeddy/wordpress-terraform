8baad9c382a353e8bd44d5be965c215da1c52f61
davidjeddy <me@davidjeddy.com>
2018-04-20 14:57:08 -0400

    ADDED description per best practice from https://www.terraform.io/docs/modules/create.html

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "wordpress_public_ip" {
+  description = "Public IP to access the web resource."
   value = "${aws_eip.wordpress_eip.public_ip}"
 }
 
 output "wordpress_dns" {
+  description = "Public DNS to access the web resource."
   value = "${aws_route53_record.www.name}"
 }
